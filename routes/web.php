511e0ed5bc246990f99dff4a04cc0a32d1af69e7
ivankajr <ivnka24@gmail.com>
2021-03-10 13:57:32 +0700

    make controller & copy template

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ContactC;
+use App\Http\Controllers\BlogC;
+use App\Http\Controllers\BlogDetailCr;
+use App\Http\Controllers\ProjectDetailC;
+use App\Http\Controllers\HomeC;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/contact', [ContactController::class, 'index'])->name('contact');
+Route::get('/blog', [BlogController::class, 'index'])->name('blog');
+Route::get('/blog-detail', [BlogDetailController::class, 'index'])->name('blog-detail');
+Route::get('/project-detail', [ProjectDetailController::class, 'index'])->name('project-detail');
+Route::get('/', [HomeController::class, 'index'])->name('index');
