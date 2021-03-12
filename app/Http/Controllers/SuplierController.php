24d8c7fcc5147d9eacbc11085ed1a9320369ac35
= <=>
2021-03-12 22:00:14 +0700

    mengisi Suplier controller

--- app/Http/Controllers/SuplierController.php
+++ app/Http/Controllers/SuplierController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Pelanggan;
 
 class SuplierController extends Controller
 {
-    //
+    public function index(){
+        return view('suplier', ['suplier' => suplier::suplier()]);
+    }
 }
