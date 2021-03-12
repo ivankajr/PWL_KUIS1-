ba285c8d0a42dd7151d13ab4f996df59ca279c23
ivankajr <ivnka24@gmail.com>
2021-03-12 13:35:28 +0700

    add index pelanggan

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,6 +59,9 @@ https://templatemo.com/tm-538-digital-trend
                         <a href="{{route('barang')}}" class="nav-link">Barang</a>
                     </li>
                     <li class="nav-item">
+                        <a href="{{route('pelanggan')}}" class="nav-link">Pelanggan</a>
+                    </li>
+                    <li class="nav-item">
                         <a href="{{route('contact')}}" class="nav-link contact">Contact</a>
                     </li>
                 </ul>
