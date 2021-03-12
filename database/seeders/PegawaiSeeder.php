04535fa3457daa3c38905c27780d2dd118569e38
= <=>
2021-03-12 21:35:08 +0700

    membuat table migrasi pegawai

--- database/seeders/PegawaiSeeder.php
+++ database/seeders/PegawaiSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\db;
 
 class PegawaiSeeder extends Seeder
 {
@@ -13,6 +14,27 @@ class PegawaiSeeder extends Seeder
      */
     public function run()
     {
-        //
+        db::table('pegawais')->insert([[
+            'id' => '1',
+            'namaPGW' => 'rico'
+            'alamat' => 'malang'
+            'noTLP' => '088768'
+        ],[
+            'id' => '2',
+            'namaPGW' => 'rina'
+            'alamat' => 'malang'
+            'noTLP' => '088769'
+        ],[
+            'id' => '3',
+            'namaPGW' => 'rani'
+            'alamat' => 'surabaya'
+            'noTLP' => '088778'
+        ],[
+            'id' => '4',
+            'namaPGW' => 'rois'
+            'alamat' => 'malang'
+            'noTLP' => '089768'
+        ]]);
+    }
     }
 }
