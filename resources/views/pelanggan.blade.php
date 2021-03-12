465f92093e5888ae993e880e7fea650498a5d960
ivankajr <ivnka24@gmail.com>
2021-03-12 13:34:36 +0700

    add view pelanggan

---
+++ resources/views/pelanggan.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+@section('content')
+<!-- pelangggan -->
+<section class="pelangggan section-padding">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-lg-12 col-12 py-5 mt-5 mb-3 text-center">
+        <h1 class="mb-4" data-aos="fade-up">pelangggan Kami</h1>
+      </div>
+
+      @foreach($pelangggan as $pelangggan)
+      <div class="col-xl-4 col-sm-12 mt-5">
+        <div class="item project-wrapper" data-aos="fade-up" data-aos-delay="100">
+            <h3 class="text-truncate">
+              <span>{{$pelangggan->nmpelanggan}}</span>
+            </h3>
+            <h3 class="text-truncate">
+              <span>{{$pelangggan->alamat}}</span>
+            </h3>
+          <!-- </div> -->
+        </div>
+      </div>
+      @endforeach
+
+    </div>
+</section>
+@endsection
