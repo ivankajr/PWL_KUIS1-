b9202e5be76ebba61e9dfaa833e0bb110aa746db
ivankajr <ivnka24@gmail.com>
2021-03-12 13:14:02 +0700

    fix contoller

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class PelangganController extends Controller
 {
-    //
+    public function index(){
+        return view('pelanggan', ['pelanggan' => pelanggan::pelanggan()]);
+    }
 }
