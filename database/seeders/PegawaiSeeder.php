10edbe75bf32d5f88d561d087d04ac2d170f18d7
= <=>
2021-03-12 21:43:01 +0700

    memperbaiki pegawai seed dan  mengirimkan pegawai seed

--- database/seeders/PegawaiSeeder.php
+++ database/seeders/PegawaiSeeder.php
@@ -16,25 +16,24 @@ class PegawaiSeeder extends Seeder
     {
         db::table('pegawais')->insert([[
             'id' => '1',
-            'namaPGW' => 'rico'
-            'alamat' => 'malang'
+            'namaPGW' => 'rico',
+            'alamat' => 'malang',
             'noTLP' => '088768'
         ],[
             'id' => '2',
-            'namaPGW' => 'rina'
-            'alamat' => 'malang'
+            'namaPGW' => 'rina',
+            'alamat' => 'malang',
             'noTLP' => '088769'
         ],[
             'id' => '3',
-            'namaPGW' => 'rani'
-            'alamat' => 'surabaya'
+            'namaPGW' => 'rani',
+            'alamat' => 'surabaya',
             'noTLP' => '088778'
         ],[
             'id' => '4',
-            'namaPGW' => 'rois'
-            'alamat' => 'malang'
+            'namaPGW' => 'rois',
+            'alamat' => 'malang',
             'noTLP' => '089768'
         ]]);
-    }
-    }
-}
+    }    }
+    
